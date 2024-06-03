7db1fe6644337c1aa24c0c5b890ebd9d6eabe407
Amit Patil <110932835+AmitPatil1302@users.noreply.github.com>
2024-06-03 21:06:33 +0530

    Update send_doubt.php

--- my-school-website/send_doubt.php
+++ my-school-website/send_doubt.php
@@ -1,10 +1,10 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $doubt = $_POST["doubt"];
-    $to = "prarthana.pathade16@gmail.com"; // Change this to your email address
+    $to = "patilamit5281@gmail.com"; // Change this to your email address
     $subject = "New Doubt";
     $message = $doubt;
-    $headers = "From: prarthanapathade123@gmail.com"; // Change this to a valid email address
+    $headers = "From: patilamit5281@gmail.com"; // Change this to a valid email address
 
     if (mail($to, $subject, $message, $headers)) {
         echo "Doubt sent successfully!";
